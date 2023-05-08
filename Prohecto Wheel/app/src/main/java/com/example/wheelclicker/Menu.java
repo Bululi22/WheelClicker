bd346b1d9623a89035e930ceb61471619bf416fb
Adrian <adrian.ferrad@gmail.com>
2023-05-08 13:22:27 +0200

    Prueba semáforo v1

--- Prohecto Wheel/app/src/main/java/com/example/wheelclicker/Menu.java
+++ Prohecto Wheel/app/src/main/java/com/example/wheelclicker/Menu.java
@@ -4,46 +4,75 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Menu extends AppCompatActivity {
 
-    BottomNavigationView nav;
-
+    private ImageView luzSemaforo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        nav = findViewById(R.id.nav);
+        luzSemaforo = findViewById(R.id.imageView3);
+        semaforo_rojo_a_verde();
+        luzSemaforo = findViewById(R.id.imageView4);
+        semaforo_rojo_a_verde();
+        luzSemaforo = findViewById(R.id.imageView5);
+        semaforo_rojo_a_verde();
+//        apagar_semaforo();
 
-        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+
+    }
+
+    private void semaforo_rojo_a_verde (){
+        new Timer().schedule(new TimerTask() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.rueda:
-                        Toast.makeText(Menu.this, "Home", Toast.LENGTH_LONG).show();
-                        break;
-
-                    case R.id.mejoras:
-                        Toast.makeText(Menu.this, "Home", Toast.LENGTH_LONG).show();
-                        break;
-
-                    case R.id.ajustes:
-                        Toast.makeText(Menu.this, "Home", Toast.LENGTH_LONG).show();
-                        break;
-                    default:
-                }
-                return true;
+            public void run() {
+                luzSemaforo.setImageResource(R.drawable.semaforo_rojo);
             }
-        });
+        }, 1000);
+    }
 
+
+//    private void semaforo_rojo_a_verde (){
+//        new Timer().schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                luzSemaforo1.setImageResource(R.drawable.semaforo_rojo);
+//                new Timer().schedule(new TimerTask() {
+//                    @Override
+//                    public void run() {
+//                        luzSemaforo2.setImageResource(R.drawable.semaforo_rojo);
+//                        new Timer().schedule(new TimerTask() {
+//                            @Override
+//                            public void run() {
+//                                luzSemaforo3.setImageResource(R.drawable.semaforo_rojo);
+//                            }
+//                        }, 1000);
+//                    }
+//                }, 1000);
+//            }
+//        }, 1000);
+//    }
+    private void apagar_semaforo (){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+//                luzSemaforo.setImageResource(R.drawable.circulo_verde);
+            }
+        }, 1000);
     }
 }
